03e474fcf2688106bf1aa279300dfb35a7164590
Prashantcoder88 <115200889+Prashantcoder88@users.noreply.github.com>
2023-01-17 18:05:12 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+//const oper = require("./oper.js");
+// OR
+
+//console.log(oper);
+// console.log(oper.add(5,5));
+
+// console.log(oper.sub(10,5));
+//console.log(add);
+const {add,sub,mul} = require("./oper.js");
+
+console.log(add(5,5));
+console.log(sub(10,5));
+console.log(mul(9,5));
